3534da1afffff991a258cdfccdc20c20967c7f3a
Svi-Kos <svitlanakos@gmail.com>
2023-02-02 19:25:42 +0200

    libs bcrypt joi jsonwebtoken

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 import todos from "./api/todos.route.js";
 
 dotenv.config();
-const { PORT = 3000, DB_HOST } = process.env;
+const { PORT = 4000, DB_HOST } = process.env;
 
 const app = express();
 
@@ -18,8 +18,6 @@ app.use((err, _, res, __) => {
   const { status = 500, message = "server error" } = err;
   res.status(status).json({ error: message });
 });
-
-// app.listen(4000);
 
 await mongoose
   .set("strictQuery", false)
